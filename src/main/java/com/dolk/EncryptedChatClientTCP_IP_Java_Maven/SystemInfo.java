4e8903148608c70f51132c3a2bf318696fa8e21e
Alex01234 <alexander.dolk@hotmail.com>
2022-08-05 19:34:58 +0200

    First commit

---
+++ src/main/java/com/dolk/EncryptedChatClientTCP_IP_Java_Maven/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.dolk.EncryptedChatClientTCP_IP_Java_Maven;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.dolk.EncryptedChatClientTCP_IP_Java_Maven {
+    requires javafx.controls;
+    exports com.dolk.EncryptedChatClientTCP_IP_Java_Maven;
+}
